ecf556fef6483564f60731773dc877de26eaf4fa
Luis F. Cruz De la Cruz <lfcruzd@gmail.com>
2013-07-09 09:04:59 -0400

    RC1.0.1 Adding batch DB loader for billerhub

---
+++ DKT-POS_Simulator/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<title> POS Simulator - GCS Systems LTD </title>
+
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <h2> tPago Prepago - Menu Principal </h2>
+        </div> <!-- end #header -->
+        <div id="nav">
+            <a href="afiliaciones.php">Afiliaciones</a><br/>
+            <a href="depositos.php">Depositos</a><br/>
+            <a href="pagos.php?paymentType=CC">Pagos de Terjetas</a><br/>
+            <a href="pagos.php?paymentType=PP">Pagos de Prestamos</a><br/>
+            <a href="pagos.php?paymentType=SS">Pagos de Servicios</a><br/>
+            <a href="retiros.php">Retiros</a><br/>
+        </div> <!-- end #nav -->
+    <div id="footer">
+	<p>Copyright &copy 2013 <a href="http://www.tpago.com.do"> tPago </a></p>
+    </div> <!-- end #footer -->
+    </div> <!-- End #wrapper -->
+</body>
+</html>
