a3ea7da596bf597f1014107ae03061b9cc92a357
lfcruzd <lfcruzd@lfcruzd-laptop>
2018-08-05 11:38:26 -0400

    [OfflinePayment] - Loader adjustment (Eliminate useless log).

--- offlinepayment/process/triggerLoader.php
+++ offlinepayment/process/triggerLoader.php
@@ -4,5 +4,4 @@ include_once '../lib/dataLoaderClass.php';
 $config = new configLoader('../config/billers_files.json');
 $filename = "../".$config->structure[$argv[1]]['inputFileDirectory'].$argv[1]."_".date(Ymd).".txt";
 $loader = new gdmLoader($filename);
-var_dump($loader);
 $loader->process();
