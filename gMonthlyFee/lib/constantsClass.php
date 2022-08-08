6e13c285b28bd7341d6e80e6e4b2885556990630
lfcruz <lfcruzd@gmail.com>
2022-08-08 13:31:19 -0400

    - New DB profile (Pools) - New constants for queries definitions - Rename generator process - Refactor pools handling for channels - Adding Pool class (TODO public/private methods)

--- gMonthlyFee/lib/constantsClass.php
+++ gMonthlyFee/lib/constantsClass.php
@@ -30,6 +30,9 @@ define("E_ACCOUNT_EXPIRED","9002");
 define("E_ACCOUNT_EXIST","8003");
 
 //Data Queries
+define("QRY_JUNCTION_AND"," and ");
+define("QRY_JUNCTION_OR"," or ");
+define("QRY_LIMIT"," limit ");
 define("QRY_GET_MF_PARAMS","select GEN_PARAMETER_NAME, PARAM_DISPLAY_NAME, PARAMETER_VALUE from  GCSGENERIC_PARAMS_M where GEN_PARAMETER_NAME in ");
 define("QRY_GET_MF_BASE","select mbp.monthlybill_payment_id, mbp.gcs_account_id, mbp.bill_due_date, mbp.monthly_bill_amount from monthly_gcs_bill_payment_l mbp inner join r_gcscustomer_account_m acc on (acc.gcs_account_id = mbp.gcs_account_id) where mbp.collector_status = '0'");
 define("QRY_FILTER_DEFAULT"," and mbp.bill_due_date > current_timestamp - interval '");
@@ -44,6 +47,11 @@ define("QRY_INTERVAL_MINUTES", "' minutes");
 define("QRY_INTERVAL_SECONDS", "' seconds");
 define("QRY_FILTER_ENGINE_BANKS"," and partner_code in ");
 define("QRY_UPDATE_BILL_CHARGE", "update monthly_gcs_bill_payment_l mbp set mbp.gcs_sequenceno = $1 where mbp.monthlybill_payment_id = $2");
+define("QRY_GET_POOL_BULK","select * from jobs where pool_id = ");
+define("QRY_FILTER_JOBS_TYPE"," job_type = ");
+define("QRY_FILTER_JOBS_CHANNEL"," channel_id = ");
+define("QRY_FILTER_JOBS_POOL"," pool_id = ");
+define("QRY_FILTER_JOBS_IS_SUBSCRIBER"," is_subscriber is true");
 
 
 
