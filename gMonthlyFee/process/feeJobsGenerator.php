ceac9858a1e784490d39a0fe908b17e083cd9e01
lcruz <lcruz@localhost>
2022-10-18 07:35:37 -0400

    gMonthlyFee - Set config load validation block.

--- gMonthlyFee/process/feeJobsGenerator.php
+++ gMonthlyFee/process/feeJobsGenerator.php
@@ -4,17 +4,12 @@ include_once 'lib/configClass.php';
 include_once 'lib/logClass.php';
 include_once 'lib/gcsObjectHandlerClass.php';
 $vChannel = $argv[1];
-$mfConfig = new gMFConfigure($vChannel);
-var_dump($mfConfig->channelGeneralParameters);
-var_dump($mfConfig->channelEngine);
 
-
-echo "************************************************************************\n";
-//$mfBills = new gMFBills('TPGDR');
-//var_dump($mfBills->getMFBills($mfConfig->channelGeneralParameters['CollectorSchedulerDays']['param_value']));
-
-//var_dump($mfConfig);
-//var_dump($mfBills);
+//Main process 
+if ($mfConfig = new gMFConfigure($vChannel)){
+     $mfBills = new gMFBills('TPGDR');
+     $mfBills->getMFBills($mfConfig->channelGeneralParameters['CollectorSchedulerDays']['param_value']);
+}
 
 
 
