7399c7af81fdf4b0f9a74a01f8d0084eaf4d9c9b
Luis Cruz <lfcruzd@gmail.com>
2025-03-09 00:01:04 -0400

    message from copilot.

--- WServer/gtClaroBiller.php
+++ WServer/gtClaroBiller.php
@@ -1,23 +1,19 @@
 <?php
 //name="EBanking" targetNamespace="http://webservices-ext.orange.com.do"
+
 class SOAPServerClass {
-    public function getContractInfo(){
-        $xmlResponse = Array("getContractInfoResponse" => Array("return" => "0","return" => "R","return" => "ORANGE"));
-        var_dump($)
+    public function getContractInfo() {
+        $xmlResponse = [
+            "getContractInfoResponse" => [
+                "return1" => "0",
+                "return2" => "R",
+                "return3" => "ORANGE"
+            ]
+        ];
+        var_dump($xmlResponse);
         return $xmlResponse;
     }
-
 }
-
-<xs:complexType name="contractInfoMsg">
-<xs:sequence>
-<xs:element minOccurs="0" name="customerType" type="xs:string" />
-<xs:element name="postpaid" type="xs:boolean" />
-<xs:element name="prepaid" type="xs:boolean" />
-</xs:sequence>
-</xs:complexType>
-
-
 
 $server = new SoapServer("orangeBiller.wsdl");
 $server->setClass('SOAPServerClass');
